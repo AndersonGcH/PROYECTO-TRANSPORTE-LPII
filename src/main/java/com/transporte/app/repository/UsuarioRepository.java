04d13a12338d506735286430bbec21e4e4a398c0
Anderson Cutipa <andersoncutipa113@gmail.com>
2024-10-08 02:35:57 -0500

    Proyecto 1.1

--- src/main/java/com/transporte/app/repository/UsuarioRepository.java
+++ src/main/java/com/transporte/app/repository/UsuarioRepository.java
@@ -1,8 +1,16 @@
 package com.transporte.app.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UsuarioRepository {
+import com.transporte.app.entity.Usuario;
 
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	@Query(value="SELECT u.idusuario,u.usuario,u.nombres,u.apellidos,u.clave,u.idrol "
+			+ "FROM usuario u where u.usuario = :usuario and u.clave = :clave",nativeQuery = true)
+	public Usuario findByUsuarioAndClave(@Param("usuario") String usuario,@Param("clave") String clave);
 }
---
+++ src/main/java/com/transporte/app/service/impl/RolServiceImpl.java
@@ -0,0 +1,22 @@
+package com.transporte.app.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.transporte.app.entity.Rol;
+import com.transporte.app.repository.RolRepository;
+import com.transporte.app.services.RolService;
+
+@Service
+public class RolServiceImpl implements RolService{
+	@Autowired
+	RolRepository rolRepository;
+
+	@Override
+	public List<Rol> getAllRol() {
+		// TODO Auto-generated method stub
+		return rolRepository.findAll();
+	}
+}
